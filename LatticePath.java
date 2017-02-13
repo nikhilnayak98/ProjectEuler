decd6a961e90673ada2176666b1055a722ab5678
Nikhil Nayak <nikhilnayak98@gmail.com>
2017-02-13 23:41:19 +0530

    Add files via upload

---
+++ LatticePath.java
@@ -0,0 +1,55 @@
+/*
+ * Author: Nikhil Ranjan Nayak
+ * Regd.no: 1641012040
+ * Branch & Sec: CSE 'F'
+ * Brief Desc:
+ */
+
+public class LatticePath {
+
+	static long arr[][] = new long[200][200];
+
+	public static void main (String[] args) {
+		long k = 2;
+		int n = 90;
+		for(int i=0;i<n;i++) {
+			arr[i][0]=k++;
+		}
+
+		k=2;
+
+		for(int j=0;j<n;j++) {
+			arr[0][j]=k++;
+		}
+
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+		System.out.println();
+
+		for(int p=1;p<n;p++) {
+			for(int i=p;i<n;i++) {
+				for(int j=i;i<n;i++) {
+					arr[i][j] = arr[i][j-1] + arr[i-1][j];
+					arr[j][i] = arr[i][j-1] + arr[i-1][j];
+					arr[i][i] = arr[i-1][i] + arr[i][i-1];
+				}
+			}
+		}
+
+		System.out.println();
+
+		for(int i=0;i<n;i++) {
+			for(int j=0;j<n;j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
